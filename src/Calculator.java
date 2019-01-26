56ebf4d83430bc15edab4b3f2208f9c49304f9a9
dariaBobil <daria.bobil@gmail.com>
2019-01-25 22:31:49 -0500

    Update substract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 
 
     public static double substract (double num1, double num2){
-
-        return num1 - num2;
+        double result = num1 - num2;
+        return result;
     }
 }
